f6b0b48bb72c2520a1169d3ec845a1aeaf63661f
Dario <dariorienhardt@gmx.de>
2015-10-21 13:59:45 +0200

    bla

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,18 +1,14 @@
-/**
- * 
- */
+import java.util.Scanner;
 
-/**
- * @author Tobias
- *
- */
 public class HelloWorld {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
 		System.out.println("Hello ASE2015 - how are you? :)");
+		String name = in.nextLine();
+		HelloUser HelloU = new HelloUser(name);
+		System.out.println("Hello " + HelloU.uName);
 	}
-
 }
+
+
